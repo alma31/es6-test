41423f933c878fff27d60533a0cdb46091233c13
alma31 <lenni31@neuf.fr>
2016-12-20 10:56:02 +0100

    modif avec step 2

--- public/app.js
+++ public/app.js
@@ -1,3 +1,17 @@
-"use strict";
+'use strict';
 
-console.log("teste");
+var tableau = [];
+document.getElementById('btn').addEventListener('click', function () {
+    var inputValue = document.getElementById('nom').value;
+    if (inputValue === "") {
+        document.getElementById('di').innerHTML = "pere noel";
+    } else {
+        var tab = document.body.appendChild(document.createElement('li')).innerHTML = inputValue;
+        var html = document.getElementById('di').innerHTML = inputValue;
+        tableau.push(inputValue);
+        var premier = tab.charAt(0).toUpperCase();
+        var maj = premier + inputValue.slice(1);
+        var ecriture = tab + maj;
+        console.log(maj);
+    }
+});
--- src/app.es6.js
+++ src/app.es6.js
@@ -1,1 +1,19 @@
-console.log("teste");
+let tableau = []
+document.getElementById('btn').addEventListener('click', () => {
+    let inputValue = document.getElementById('nom').value;
+    if (inputValue === "") {
+    	document.getElementById('di').innerHTML = "pere noel"
+    }
+    else { 
+        let tab = document.body.appendChild(document.createElement('li')).innerHTML = inputValue;
+        let html = document.getElementById('di').innerHTML = inputValue;
+        tableau.push(inputValue);
+        let premier = tab.charAt(0).toUpperCase();
+        let maj = premier + inputValue.slice(1);
+        let ecriture = tab + maj
+        console.log(maj);
+
+    }
+
+});
+
