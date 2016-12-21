ad0cfd73c01ce9bebafd143fd342f7301ac6ec40
alma31 <lenni31@neuf.fr>
2016-12-21 15:46:35 +0100

    modif de 16h

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var tableau = [];
-document.getElementById('btn').addEventListener('click', function () {
+var test = document.getElementById('btn').addEventListener('click', function () {
     var inputValue = document.getElementById('nom').value;
     if (inputValue === "") {
         document.getElementById('di').innerHTML = "pere noel";
@@ -9,8 +9,25 @@ document.getElementById('btn').addEventListener('click', function () {
         var tab = inputValue;
         tableau.push(inputValue);
         var premier = tab.charAt(0).toUpperCase();
-        var maj = premier + inputValue.slice(1);
+        var maj = premier + inputValue.slice(1).toLowerCase();
         var ecriture = document.body.appendChild(document.createElement('li')).innerHTML = maj;
         var html = document.getElementById('di').innerHTML = maj;
+    }
+});
+document.getElementById('srcbtn').addEventListener('click', function () {
+    var tablo = [];
+    var filt = document.getElementById('filt');
+    var valeurInput = document.getElementById('src').value.toLowerCase();
+    var longeur = valeurInput.length;
+    for (var i = 0; i < tableau.length; i++) {
+
+        var element = tableau[i];
+        var filter = element.indexOf(valeurInput);
+        console.log(filter);
+        if (filter === 0) {
+            tablo.push(filter);
+            console.log(tablo);
+            filt.innerHTML = element;
+        }
     }
 });
--- src/app.es6.js
+++ src/app.es6.js
@@ -1,5 +1,5 @@
-let tableau = []
-document.getElementById('btn').addEventListener('click', () => {
+const tableau = []
+let test = document.getElementById('btn').addEventListener('click',() => {
     let inputValue = document.getElementById('nom').value;
     if (inputValue === "") {
     	document.getElementById('di').innerHTML = "pere noel"
@@ -8,10 +8,32 @@ document.getElementById('btn').addEventListener('click', () => {
         let tab = inputValue;
         tableau.push(inputValue);
         let premier = tab.charAt(0).toUpperCase();
-        let maj = premier + inputValue.slice(1);
+        let maj = premier + inputValue.slice(1).toLowerCase();
         let ecriture = document.body.appendChild(document.createElement('li')).innerHTML = maj
         let html = document.getElementById('di').innerHTML = maj;
     }
 
 });
+document.getElementById('srcbtn').addEventListener('click', () => {
+    let tablo = [];
+    let filt = document.getElementById('filt');
+    let valeurInput = document.getElementById('src').value.toLowerCase();
+    let longeur = valeurInput.length;
+    for(var i = 0; i < tableau.length; i++){
+
+        let element = tableau[i];
+        let filter = element.indexOf(valeurInput);
+        console.log(filter);
+        if (filter === 0){
+            tablo.push(filter);
+            console.log(tablo);
+            filt.innerHTML = element;
+        }
+    }
+
+
+
+
+});
+
 
